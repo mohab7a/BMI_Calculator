94a6cb004ea5d168f6ef52055e7cdb89591d31f8
Mohab Sarhan <mohab.sarhan17@users.noreply.github.com>
2021-05-16 04:59:07 +0200

    finish app

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -157,7 +157,7 @@ class _BmiScreenState extends State<BmiScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey),
+                        color: Colors.grey[400]),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -206,7 +206,7 @@ class _BmiScreenState extends State<BmiScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey),
+                        color: Colors.grey[400]),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
